f24146207e25bbdeb1c8c0e0d5b6d434441be083
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-24 07:35:21 +0900

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,40 @@
 package PACKAGE_NAME;
 
 public class Car {
+
+    //Base Class
+
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.engine = true;
+        this.wheels = 4;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine(){
+        return getClass().getSimpleName() + " -> startEngine()"  ;
+    }
+
+    public String accelerate(){
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    public String  brake(){
+        return getClass().getSimpleName() + " -> brake()";
+    }
+
 }
